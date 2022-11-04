88a16350b4f4d35a59f3edb39b8ebf7095f28bae
Matheus Sant'ana <matheusm.santana@outlook.com>
2022-11-04 22:19:22 +0100

    Setup test configuration to tear down the test database after each test

--- wookie_books/tests/test_book_views.py
+++ wookie_books/tests/test_book_views.py
@@ -5,7 +5,13 @@ from wookie_books.main import app
 client = TestClient(app)
 
 
-def test_read_books():
+def test_read_books(test_db):
     response = client.get("/books/")
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_darth_vader_can_not_create_book(test_db):
+    response = client.post("/users/1/books/")
     assert response.status_code == 200
     assert response.json() == []
--- wookie_books/tests/test_user_views.py
+++ wookie_books/tests/test_user_views.py
@@ -1,11 +1,36 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from wookie_books.main import app
+from ..main import app
+from ..models import Base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
 
 client = TestClient(app)
 
 
-def test_read_users():
-    response = client.get("/users/")
-    assert response.status_code == 200
-    assert response.json() == []
+def test_create_user(test_db):
+    response = client.post(
+        "/users/",
+        json={"username": "deadpool", "password": "chimichangas4life"},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["username"] == "deadpool"
+    assert "id" in data
+    user_id = data["id"]
+
+    response = client.get(f"/users/{user_id}")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["username"] == "deadpool"
+    assert data["id"] == user_id
