658ef441e7edd2ca7873156cc0d4d99f2f1df7df
Matheus Sant'ana <matheusm.santana@outlook.com>
2022-11-05 00:37:48 +0100

    Fix book tests.

--- wookie_books/tests/test_book_views.py
+++ wookie_books/tests/test_book_views.py
@@ -1,17 +1,10 @@
-from fastapi.testclient import TestClient
-
-from wookie_books.main import app
-
-client = TestClient(app)
-
-
-def test_read_books(test_db):
+def test_read_books(client):
     response = client.get("/books/")
     assert response.status_code == 200
     assert response.json() == []
 
 
-def test_darth_vader_can_not_create_book(test_db):
+def test_darth_vader_can_not_create_book(client):
     response = client.post("/users/1/books/")
     assert response.status_code == 200
     assert response.json() == []
